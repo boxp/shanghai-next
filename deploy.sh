eb922b26926aae674fe35ff77271eab46e542d21
boxp <tiyotiyouda@gmail.com>
2017-03-23 02:11:27 +0900

    :pencil2: Reflect gcloud command error

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 # Exit on any error
 set -e
 
-sudo /opt/google-cloud-sdk/bin/gcloud docker push asia.gcr.io/${PROJECT_NAME}/shanghai
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/shanghai
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl apply -f deployment.yml
