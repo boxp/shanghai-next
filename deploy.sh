fbc5223283fc4ec6ad1537d764739b3afc24aa7c
boxp <tiyotiyouda@gmail.com>
2017-03-24 01:01:33 +0900

    :pencil2: Refactor deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,4 @@ set -e
 
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/shanghai:latest
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl apply -f k8s/deployment.yml
-kubectl apply -f k8s/service.yml
-kubectl apply -f k8s/ingress.yml
+kubectl apply -f k8s/
