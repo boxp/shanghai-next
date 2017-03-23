88de0246735a4ff53825282c0f9a7350767532e9
boxp <tiyotiyouda@gmail.com>
2017-03-24 00:50:36 +0900

    :pencil2: Add ingress, service

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,6 @@ set -e
 
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/shanghai:latest
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl apply -f deployment.yml
+kubectl apply -f k8s/deployment.yml
+kubectl apply -f k8s/service.yml
+kubectl apply -f k8s/ingress.yml
