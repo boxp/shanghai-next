3456df65b128b6e62094ae1b0e16b13e00d68182
boxp <tiyotiyouda@gmail.com>
2017-03-25 23:19:59 +0900

    :pencil2: Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,9 @@ set -e
 
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/shanghai:latest
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+kubectl apply -f k8s/lego/00-namespace.yml
 kubectl apply -f k8s/configmap.yml
-kubectl apply -f k8s/lego/
+kubectl apply -f k8s/lego/deployment.yml
 kubectl apply -f k8s/deployment.yml
 kubectl apply -f k8s/service.yml
 kubectl apply -f k8s/ingress.yml
