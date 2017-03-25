7367d24314d21cb7f3e12678517214ed5fa8a7ea
boxp <tiyotiyouda@gmail.com>
2017-03-26 05:29:38 +0900

    :pencil2: Revert

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ set -e
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/shanghai:latest
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl apply -f k8s/lego/00-namespace.yml
-kubectl apply -f k8s/nginx/00-namespace.yml
-kubectl apply -f k8s/nginx/default-deployment.yml
-kubectl apply -f k8s/nginx/default-service.yml
-kubectl apply -f k8s/nginx/configmap.yml
-kubectl apply -f k8s/nginx/service.yml
-kubectl apply -f k8s/nginx/deployment.yml
+# kubectl apply -f k8s/nginx/00-namespace.yml
+# kubectl apply -f k8s/nginx/default-deployment.yml
+# kubectl apply -f k8s/nginx/default-service.yml
+# kubectl apply -f k8s/nginx/configmap.yml
+# kubectl apply -f k8s/nginx/service.yml
+# kubectl apply -f k8s/nginx/deployment.yml
 kubectl apply -f k8s/configmap.yml
 kubectl apply -f k8s/lego/deployment.yml
 kubectl apply -f k8s/deployment.yml
