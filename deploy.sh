55c593be630248390f1f9fa6930a3450376aeb17
boxp <tiyotiyouda@gmail.com>
2017-03-29 19:17:02 +0900

    Fix deploy container tag

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,12 @@
 # Exit on any error
 set -e
 
-sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/shanghai:latest
+for f in k8s/*.yml
+do
+	envsubst < $f > "generated-$(basename $f)"
+done
+
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/shanghai:$CIRCLE_SHA1
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl apply -f k8s/lego/00-namespace.yml
 # kubectl apply -f k8s/nginx/00-namespace.yml
@@ -12,8 +17,8 @@ kubectl apply -f k8s/lego/00-namespace.yml
 # kubectl apply -f k8s/nginx/configmap.yml
 # kubectl apply -f k8s/nginx/service.yml
 # kubectl apply -f k8s/nginx/deployment.yml
-kubectl apply -f k8s/configmap.yml
+kubectl apply -f generated-configmap.yml
 kubectl apply -f k8s/lego/deployment.yml
-kubectl apply -f k8s/deployment.yml
-kubectl apply -f k8s/service.yml
-kubectl apply -f k8s/ingress.yml
+kubectl apply -f generated-deployment.yml
+kubectl apply -f generated-service.yml
+kubectl apply -f generated-ingress.yml
