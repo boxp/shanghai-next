87cf20677eae20930334147a6467e7a77dba1584
boxp <tiyotiyouda@gmail.com>
2017-12-17 23:37:16 +0900

    I'm Test.

--- src/shanghai/view/app.cljs
+++ src/shanghai/view/app.cljs
@@ -4,18 +4,7 @@
 
 (def icon-size 120)
 
-(defn app 
+(defn app
   [owner links data]
   (html
-    [:div
-      [:div#cmd
-        [:code (->> data :buffer)]
-        [:span "▮"]]
-      [:div#links {:style 
-                    {:width (str 
-                              (* icon-size (+ (* (count links) 2) 1)) 
-                              "px")}}
-        [:div {:class "bracket"} "'("]
-        (->> links (map #(link->box % owner)) 
-             (interpose [:div {:class "bracket"} " "]))
-        [:div {:class "bracket"} ")"]]]))
+    [:div "This is test!!!!!!"]))
